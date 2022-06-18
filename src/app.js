c12206c665cf69598a64470d9ba10eaf939ae44e
Kent Zhang <kent@Kents-MacBook-Pro.local>
2022-06-18 01:06:31 -0700

    Add test images to public directory

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const port = 8080;
 
 const server = http.createServer(function (req,res){
     console.log("Request occured");
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('index.html', function(error,data){
+    res.writeHead(200, {'Content-Type': 'image/jpeg'});
+    fs.readFile('public/test.jpg', function(error,data){
         if(error){
             res.writeHead(404);
             res.write("Error 404: Not Found");
